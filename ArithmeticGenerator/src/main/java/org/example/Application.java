a97b7449a0015b013c1a3bfa35a4e45860539511
crystral <1023712219@qq.com>
2021-10-23 10:15:43 +0800

    项目结构初始化

---
+++ ArithmeticGenerator/src/main/java/org/example/Application.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ *
+ * @author Crystry
+ * @date 2021/10/23 10:07
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("项目结构初始化");
+    }
+}
---
+++ ArithmeticGenerator/src/test/java/org/example/ApplicationTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ *
+ * @author Crystry
+ * @date 2021/10/23 10:11
+ */
+public class ApplicationTest {
+}
